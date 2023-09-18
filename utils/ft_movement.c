81132a801f4b02b379ecfd4e48ab5a414fcfe77b
Renata Inacio <rinacio@c2r8p4.42sp.org.br>
2023-09-18 18:57:16 -0300

    fix player rotation

--- utils/ft_movement.c
+++ utils/ft_movement.c
@@ -6,7 +6,7 @@
 /*   By: rinacio <rinacio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:32:56 by rinacio           #+#    #+#             */
-/*   Updated: 2023/09/18 18:16:43 by rinacio          ###   ########.fr       */
+/*   Updated: 2023/09/18 18:56:25 by rinacio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 void	ft_move_player(t_game *game, int keysym)
 {	
 	if (keysym == XK_A || keysym == XK_a)
-		ft_update_player_position(game, -PI / 2);
-	else if (keysym == XK_W || keysym == XK_w)
-		ft_update_player_position(game, 0);
-	else if (keysym == XK_D || keysym == XK_d)
 		ft_update_player_position(game, PI / 2);
+	else if (keysym == XK_W || keysym == XK_w)
+		ft_update_player_position(game, PI);
+	else if (keysym == XK_D || keysym == XK_d)
+		ft_update_player_position(game, - PI / 2);
 	else if (keysym == XK_S || keysym == XK_s)
-		ft_update_player_position(game, -PI);
+		ft_update_player_position(game, 0);
 }
 
 void	ft_rotate_player(t_game *game, int keysym)
 {
 	if (keysym == XK_Right)
-		game->player.angle - ROT_SPEED;
+		game->player.angle += ROT_SPEED;
 	else if (keysym == XK_Left)
-		game->player.angle + ROT_SPEED;
+		game->player.angle -= ROT_SPEED;
 	return ;
 }
 
@@ -39,7 +39,7 @@ void	ft_update_player_position(t_game *game, float angle)
 
 	pos.x = game->player.position.x + SPEED * cos(game->player.angle + angle);
 	pos.y = game->player.position.y + SPEED * sin(game->player.angle + angle);
-	if (game->map.map[(int)pos.y][(int)pos.x] != '0')
+	if (game->map.map[(int)pos.y][(int)pos.x] == '1')
 		return ;
 	game->player.position.x = pos.x;
 	game->player.position.y = pos.y;
