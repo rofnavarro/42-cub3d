34eb717c0bf9b0bbab54cf75bc8bacd73e08ad3f
rinacio <revazgauska@gmail.com>
2023-09-19 02:35:22 +0200

    quick fix movement direction

--- utils/ft_movement.c
+++ utils/ft_movement.c
@@ -17,11 +17,11 @@ void	ft_move_player(t_game *game, int keysym)
 	if (keysym == XK_A || keysym == XK_a)
 		ft_update_player_position(game, PI / 2);
 	else if (keysym == XK_W || keysym == XK_w)
-		ft_update_player_position(game, PI);
+		ft_update_player_position(game, 0);
 	else if (keysym == XK_D || keysym == XK_d)
 		ft_update_player_position(game, - PI / 2);
 	else if (keysym == XK_S || keysym == XK_s)
-		ft_update_player_position(game, 0);
+		ft_update_player_position(game, PI);
 }
 
 void	ft_rotate_player(t_game *game, int keysym)
@@ -38,7 +38,7 @@ void	ft_update_player_position(t_game *game, float angle)
 	t_point	pos;
 
 	pos.x = game->player.position.x + SPEED * cos(game->player.angle + angle);
-	pos.y = game->player.position.y + SPEED * sin(game->player.angle + angle);
+	pos.y = game->player.position.y - SPEED * sin(game->player.angle + angle);
 	if (game->map.map[(int)pos.y][(int)pos.x] == '1')
 		return ;
 	game->player.position.x = pos.x;
